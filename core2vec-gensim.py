cdb69a1ebc00161fc6d099fd087ad264e6099a71
Christopher Kittel <web@christopherkittel.eu>
2019-01-23 15:37:19 +0100

    switched to word2vec only model; doc2vec exceeds available RAM

--- core2vec-gensim.py
+++ core2vec-gensim.py
@@ -1,29 +1,30 @@
 import os
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models import Word2Vec
 from nltk.corpus import stopwords
 
 
 data_folder_name = '/home/chris/data/CORE/interim'
-window_size = 5
 
 stops = stopwords.words('english')
 
-print("Loading data")
+# print("Loading data")
+# with open(os.path.join(data_folder_name, "documents_en")) as infile:
+#     docs = ([t for t in doc.lower().split() if t not in stops]
+#             for doc in infile)
+
+model = Word2Vec(size=300, window=8,  # w2v params
+                 min_count=20,  # vocab params
+                 sg=1, hs=0, negative=10,  # loss & sampling params
+                 workers=10, iter=1)
+print("Building vocabulary")
+model.build_vocab(corpus_file=os.path.join(data_folder_name, "documents_en"))
+print("Starting training")
 with open(os.path.join(data_folder_name, "documents_en")) as infile:
     docs = ([t for t in doc.lower().split() if t not in stops]
             for doc in infile)
-    tagged_docs = [TaggedDocument(doc, [tag])
-                   for tag, doc in enumerate(docs)
-                   if len(doc) > window_size]
-total_examples = len(tagged_docs)
-
-model = Doc2Vec(vector_size=300, window=window_size,
-                min_count=50, max_vocab_size=100000,
-                dbow_words=0,
-                dm=1, workers=10, hs=0, negative=10)
-print("Building vocabulary")
-model.build_vocab(tagged_docs)
-print("Starting training")
-model.train(tagged_docs, total_examples=total_examples, epochs=1)
-model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
-model.save("models/core2vec_model_en")
+model.train(corpus_file=os.path.join(data_folder_name, "documents_en"),
+            total_examples=model.corpus_count,
+            total_words=len(model.wv.vocab),
+            epochs=1)
+model = model.wv
+model.save("models/w2v_model_en")
